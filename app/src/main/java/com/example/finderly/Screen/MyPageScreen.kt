5f3c048ead119691768a20725a264774f528f34e
MinseoSong <mins041600@naver.com>
2024-05-28 20:03:54 +0900

    마이페이지 구현 완료

--- app/src/main/java/com/example/finderly/Screen/MyPageScreen.kt
+++ app/src/main/java/com/example/finderly/Screen/MyPageScreen.kt
@@ -1,6 +1,7 @@
 package com.example.finderly.Screen
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -12,19 +13,24 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.finderly.Data.MyFindItem
+import com.example.finderly.Data.MyPost
 import com.example.finderly.R
 
 @Composable
@@ -52,7 +58,8 @@ fun Appbar(
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(80.dp),
+                    .height(80.dp)
+                    .background(color = Color.White),
                 horizontalArrangement = Arrangement.SpaceBetween,
                 verticalAlignment = Alignment.CenterVertically
             ){
@@ -119,7 +126,7 @@ fun Appbar(
                     verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally,
                     modifier = Modifier
-                        .padding(end =50.dp)
+                        .padding(end = 50.dp)
                         .clickable {
                             navController.navigate("MyPage")
                         }
@@ -141,8 +148,119 @@ fun Appbar(
 }
 
 @Composable
+fun MyPageList(
+    items: List<MyPost>,
+    TopTitle: String
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(210.dp)
+            .padding(15.dp)
+    ) {
+        Text(
+            text = TopTitle,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    color = colorResource(id = R.color.lightgreen),
+                    shape = RoundedCornerShape(20.dp)
+                )
+        ){
+            Column(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                items.take(2).forEach{
+                    Row (
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(start = 15.dp, end = 15.dp, top = 12.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ){
+                        Text(
+                            text = "${it.title}",
+                            fontSize = 13.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                        Image(
+                            painter = painterResource(id = R.drawable.ic_more_green),
+                            contentDescription = "더보기",
+                            modifier = Modifier
+                                .size(13.dp)
+                                .clickable {
+                                    // 해당 게시물 상세 페이지로 이동
+                                }
+                        )
+                    }
+
+                    Text(
+                        text = "${it.detail}",
+                        fontSize = 12.sp,
+                        color = colorResource(id = R.color.field_text_gray),
+                        modifier = Modifier.padding(start = 15.dp)
+                    )
+                }
+                Spacer(modifier = Modifier.height(10.dp))
+                Divider(
+                    color = colorResource(id = R.color.field_border_gray),
+                    modifier = Modifier.padding(start = 15.dp, end = 15.dp)
+                )
+                Row(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .clickable {
+                            // 분실물, 습득물 게시글 페이지로 이동
+                        },
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = "더보기",
+                        fontSize = 11.sp,
+                        color = colorResource(id = R.color.field_text_gray)
+                    )
+                    Spacer(modifier = Modifier.width(5.dp))
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_more_black),
+                        contentDescription = "더보기 버튼",
+                        modifier = Modifier.size(13.dp)
+                    )
+
+                }
+            }
+        }
+    }
+}
+@Composable
 fun MyPageScreen(navController: NavHostController) {
     val scrollstate = rememberScrollState()
+
+    // 습득물 예시 데이터 리스트
+    val MyFindItems = listOf(
+        MyFindItem("AirPods Pro 2", "건대입구역","자양파출소"),
+        MyFindItem("AirPods Pro 3", "건대입구역","자양파출소"),
+        MyFindItem("AirPods Pro 4", "건대입구역","자양파출소"),
+        MyFindItem("AirPods Pro 5", "건대입구역","자양파출소")
+    )
+
+    // 분실물 게시글 예시 데이터 리스트
+    val MyLostPost = listOf(
+        MyPost("에어팟","에어팟 잃어버렸어요"),
+        MyPost("아이폰","아이폰 잃어버렸어요")
+    )
+
+    // 습득물 게시글 예시 데이터 리스트
+    val MyFindPost = listOf(
+        MyPost("에어팟","에어팟 찾았어요"),
+        MyPost("아이폰","아이폰 찾았어요")
+    )
+
     Column (
         modifier = Modifier
             .fillMaxSize()
@@ -216,6 +334,94 @@ fun MyPageScreen(navController: NavHostController) {
                 )
             }
         }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(210.dp)
+                .padding(15.dp)
+        ) {
+            Text(
+                text = "내 습득물",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        color = colorResource(id = R.color.lightgreen),
+                        shape = RoundedCornerShape(20.dp)
+                    )
+            ){
+                Column(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    MyFindItems.take(2).forEach{
+                        Row (
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(start = 15.dp, end = 15.dp, top = 12.dp),
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ){
+                            Text(
+                                text = "${it.title}",
+                                fontSize = 13.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                            Image(
+                                painter = painterResource(id = R.drawable.ic_more_green),
+                                contentDescription = "더보기",
+                                modifier = Modifier
+                                    .size(13.dp)
+                                    .clickable {
+                                        // 해당 게시물 상세 페이지로 이동
+                                    }
+                            )
+                        }
+
+                        Text(
+                            text = "습득지역 : ${it.location} / 보관장소 : ${it.storagePlace}",
+                            fontSize = 12.sp,
+                            color = colorResource(id = R.color.field_text_gray),
+                            modifier = Modifier.padding(start = 15.dp)
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(10.dp))
+                    Divider(
+                        color = colorResource(id = R.color.field_border_gray),
+                        modifier = Modifier.padding(start = 15.dp, end = 15.dp)
+                    )
+                    Row(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .clickable {
+                                       //습득물 페이지로 이동
+                            },
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Text(
+                            text = "더보기",
+                            fontSize = 11.sp,
+                            color = colorResource(id = R.color.field_text_gray)
+                        )
+                        Spacer(modifier = Modifier.width(5.dp))
+                        Image(
+                            painter = painterResource(id = R.drawable.ic_more_black),
+                            contentDescription = "더보기 버튼",
+                            modifier = Modifier.size(13.dp)
+                        )
+
+                    }
+                }
+            }
+        }
+
+        MyPageList(items = MyLostPost, TopTitle = "내 분실물 게시글")
+        MyPageList(items = MyFindPost, TopTitle = "내 습득물 게시글")
+        Spacer(modifier = Modifier.height(80.dp))
     }
 
     Appbar(
