89657e40c58b95e4545b8e89ecc5ee5deef28b7e
Youngbin Ko <66235935+youngbin0417@users.noreply.github.com>
2024-05-30 00:06:46 +0900

    google map (uncerfitied yet)

--- app/src/main/java/com/example/finderly/MainActivity.kt
+++ app/src/main/java/com/example/finderly/MainActivity.kt
@@ -8,6 +8,13 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.example.finderly.ui.theme.FinderlyTheme
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,3 +32,4 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
