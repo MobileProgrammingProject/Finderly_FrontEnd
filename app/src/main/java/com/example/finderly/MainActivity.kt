257774d5910eb4d402670ccfe2b97fc9c7da084f
MinseoSong <mins041600@naver.com>
2024-06-14 15:03:47 +0900

    카카오 sdk 사용 오류 해결 완료

--- app/src/main/java/com/example/finderly/MainActivity.kt
+++ app/src/main/java/com/example/finderly/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.finderly
 
+import android.content.ContentValues.TAG
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,17 +11,21 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.media3.common.util.Log
 import androidx.media3.common.util.UnstableApi
 import androidx.navigation.compose.rememberNavController
 import com.example.finderly.Navigate.NavGraph
 import com.example.finderly.ui.theme.FinderlyTheme
 import com.kakao.sdk.common.KakaoSdk
+import com.kakao.sdk.common.util.Utility
 
 class MainActivity : ComponentActivity() {
     @OptIn(UnstableApi::class)
     @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // log에 나온 keyhash 값 단톡에 남겨주세요! + 애뮬레이터 꼭 API 30 이하인 걸로 써주세요!
+        Log.d(TAG, "keyhash : ${Utility.getKeyHash(this)}")
 
         // Kakao SDK 초기화
         KakaoSdk.init(this, "b63c9474cf80866f15fb0001087ca551")
